6b00ae0919c7a78d0da30615a17b06e0e8bf211c
Grigor Yankov <yankovweb@outlook.com>
2022-12-27 00:11:59 +0200

    [Fix]Typo

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ExpenseDate } from './ExpenseDate';
 import { Card } from '../UI/Card';
 import './ExpenseItem.css';
 export const ExpenseItem = (props) => {
-  const [useTitle, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Upadated');
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{useTitle}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
