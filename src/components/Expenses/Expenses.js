fcf6bc682984a82fc0ece23f430f4e7c07ff00ab
Grigor Yankov <yankovweb@outlook.com>
2022-12-27 15:59:53 +0200

    [Update]Create_new_component_give_it_style_and_move_logic_to_It

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
+import { ExpensesList } from './ExpensesList';
 
-import { ExpenseItem } from './ExpenseItem';
 import { Card } from '../UI/Card';
 import './Expenses.css';
 import { ExpensesFilter } from './ExpensesFilter';
@@ -16,24 +16,11 @@ export const Expenses = (props) => {
     return expense.date.getFullYear().toString() === selectedYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((items) => (
-      <ExpenseItem
-        key={items.id}
-        title={items.title}
-        amount={items.amount}
-        date={items.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={selectedYear} yearUp={fiteredHandler} />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
